2e2a96fff3b6901d793453bd1aa9fd1314cf1dd4
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-02-12 02:31:29 +0700

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
-import axios from 'axios';
 import YoutubePlayer from "./components/YoutubePlayer";
 import SongList from "./components/SongList";
 import CustomPlayer from "./components/CustomPlayer";
+import Lyrics from "./components/Lyrics";
 import { useTheme } from "./contexts/ThemeContext";
 import { songs } from "./data";
 import './App.scss'
@@ -12,8 +12,6 @@ function App() {
   const [player, setPlayer] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
-  const [lyrics, setLyrics] = useState(null);
-  const [currentLyric, setCurrentLyric] = useState("");
   const intervalRef = useRef(null);
 
   const { theme, changeTheme } = useTheme();
@@ -82,72 +80,12 @@ function App() {
 
   useEffect(() => clearInterval(intervalRef.current), []);
 
-  useEffect(() => {
-    const fetchLyrics = async () => {
-      if (currentSong) {
-        try {
-          // const url = `https://lrclib.net/api/get?artist_name=${currentSong.artist}&track_name=${currentSong.title}`;
-          const artistName = encodeURIComponent(currentSong.artist);
-          const trackName = encodeURIComponent(currentSong.title);
-          const url = `https://lrclib.net/api/get?artist_name=${artistName}&track_name=${trackName}`;
-          console.log(url);
-          const response = await axios.get(url);
-
-          if (response.data.syncedLyrics) {
-            console.log(response.data.syncedLyrics);
-            setLyrics(response.data.syncedLyrics);
-          } else {
-            const searchUrl = `https://lrclib.net/api/search?artist_name=${artistName}&track_name=${trackName}`;
-            console.log(searchUrl);
-            const searchResponse = await axios.get(searchUrl);
-
-            searchResponse.data.forEach((result) => {
-              if (result.syncedLyrics) {
-                setLyrics(result.syncedLyrics);
-                return;
-              }
-            });
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    };
-    fetchLyrics();
-  }, [currentSong]);
-
-  useEffect(() => {
-    let lyricToDisplay = "";
-    if (lyrics) {
-      const startTime = currentSong.preciseStart ?? currentSong.start;
-      const endTime = currentSong.preciseEnd ?? currentSong.end;
-      const lines = lyrics.split("\n");
-      const desiredLines = lines.filter(line => {
-        const match = line.match(/\[(\d{2}):(\d{2}\.\d{2})\](.*)/);
-        if (match) {
-          const minutes = parseInt(match[1], 10);
-          const seconds = parseFloat(match[2]);
-          const time = minutes * 60 + seconds;
-          return time >= startTime && time <= endTime;
-        }
-        return false;
-      });
-      lyricToDisplay = desiredLines.map(line => line.replace(/\[\d{2}:\d{2}\.\d{2}\]/, "")).join("\n");
-
-      setCurrentLyric(lyricToDisplay);
-    }
-  }, [lyrics, currentTime]);
-
   return (
     <div className={`app ${theme.className}`}>
       <div className="app-container">
         <h1 className="description">hardest hitting Taylor Swift bridges</h1>
         <SongList songs={songs} setCurrentSong={setCurrentSong} />
-        <div className="lyrics-container">
-        {currentLyric && (
-          <pre>{currentLyric}</pre>
-        )}
-        </div>
+        <Lyrics currentSong={currentSong} />
         {currentSong && (
           <CustomPlayer currentSong={currentSong} isPlaying={isPlaying} handlePlayPause={handlePlayPause} currentTime={currentTime} />
         )}
