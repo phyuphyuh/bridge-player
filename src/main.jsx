f8ca090b641e4c25db7c0fb2565f469f776c6778
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-02-11 21:33:12 +0700

    fixed deprecated sass import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from './contexts/ThemeContext'
-import './index.css'
+import './index.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
